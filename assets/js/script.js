b49c35f7b9bf2909d3588f080a45d17569933384
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-09-15 18:56:13 -0300

    Concluindo script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 function changeImg (imagem) {
     document.querySelector('.pepsi').src = imagem;
 }
+
+function changeBgColor (color) {
+    const changeBg = document.querySelector('.secao');
+    changeBg.style.backgroundColor = color;
+}
